f77224c1cd8f457852f3f9c7681c1df2c2a88763
Mark Riley <mark@eucleo.com>
2015-05-27 14:53:29 -0400

    minor cleanup

--- src/com/chrulri/lsusb/UsbAdapter.java
+++ src/com/chrulri/lsusb/UsbAdapter.java
@@ -21,6 +21,8 @@ package com.chrulri.lsusb;
 import android.content.Context;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbManager;
+import android.os.Build;
+import android.os.Build.VERSION;
 import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,49 +33,57 @@ import org.w3c.dom.Text;
 
 final class UsbAdapter extends ArrayAdapter<UsbDevice> {
 
-    private final UsbManager manager;
-    private final LayoutInflater inflater;
+	private static final StringBuilder tmpBuf = new StringBuilder();
+	private final UsbManager manager;
+	private final LayoutInflater inflater;
 
-    public UsbAdapter(Context context) {
-        super(context, 0);
-        this.manager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
-        this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+	public UsbAdapter(Context context) {
+		super(context, 0);
+		this.manager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+		this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
 
-    public void loadDevices() {
-        setNotifyOnChange(false);
-        clear();
-        addAll(manager.getDeviceList().values());
-        notifyDataSetChanged();
-    }
+	public void loadDevices() {
+		setNotifyOnChange(false);
+		clear();
+		addAll(manager.getDeviceList().values());
+		notifyDataSetChanged();
+	}
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            convertView = inflater.inflate(R.layout.listitem_usb, parent, false);
-        }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.listitem_usb, parent, false);
+		}
 
-        UsbDevice device = getItem(position);
-        TextView manufacturer = (TextView) convertView.findViewById(R.id.manufacturer);
-        TextView product = (TextView) convertView.findViewById(R.id.product);
-        TextView vid = (TextView) convertView.findViewById(R.id.vid);
-        TextView pid = (TextView) convertView.findViewById(R.id.pid);
-        TextView path = (TextView) convertView.findViewById(R.id.path);
-        TextView serial = (TextView) convertView.findViewById(R.id.serial);
+		UsbDevice device = getItem(position);
+		TextView manufacturer = (TextView) convertView.findViewById(R.id.manufacturer);
+		TextView product = (TextView) convertView.findViewById(R.id.product);
+		TextView vid = (TextView) convertView.findViewById(R.id.vid);
+		TextView pid = (TextView) convertView.findViewById(R.id.pid);
+		TextView path = (TextView) convertView.findViewById(R.id.path);
+		TextView serial = (TextView) convertView.findViewById(R.id.serial);
 
-		manufacturer.setText(device.getManufacturerName());
-        product.setText(device.getProductName());
-        vid.setText(zeroPad(Integer.toHexString(device.getVendorId())));
-        pid.setText(zeroPad(Integer.toHexString(device.getProductId())));
-        path.setText(device.getDeviceName());
-        serial.setText(device.getSerialNumber());
-        return convertView;
-    }
+		vid.setText(zeroPad(Integer.toHexString(device.getVendorId()), 4));
+		pid.setText(zeroPad(Integer.toHexString(device.getProductId()), 4));
+		path.setText(device.getDeviceName());
+		if(VERSION.SDK_INT >= 21) {
+			manufacturer.setText(device.getManufacturerName());
+			product.setText(device.getProductName());
+			serial.setText(device.getSerialNumber());
+		}
+		return convertView;
+	}
 
-    private static String zeroPad(String text) {
-        while(text.length() < 4) {
-            text = "0" + text;
-        }
-        return text;
-    }
+	private static String zeroPad(String text, int to) {
+		if(text.length() < to) {
+			tmpBuf.setLength(0);
+			tmpBuf.append(text);
+			while (tmpBuf.length() < to) {
+				tmpBuf.insert(0, "0");
+			}
+			text = tmpBuf.toString();
+		}
+		return text;
+	}
 }
